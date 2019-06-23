f041ab4a740d122eba454348262cb00cc2c038f2
zDonik <tokhirovdoniyor@gmail.com>
2019-06-23 12:39:01 +0500

    Fixed update issues related to time

--- server_main.cpp
+++ server_main.cpp
@@ -1,21 +1,15 @@
 #include "world.h"
 
-#define UPDATE_TIMESTEP 1
+#define SLEEP_TIME 1
 
 int main()
 {
     World world;
     sf::Clock clock;
-    sf::Time elapsed = clock.restart();
 
     while (world.isRunning()) {
-        if (elapsed.asMilliseconds() >= UPDATE_TIMESTEP) {
-            world.update(clock.restart());
-            elapsed -= sf::milliseconds(UPDATE_TIMESTEP);
-            sf::sleep(sf::milliseconds(UPDATE_TIMESTEP));
-        }
-
-        elapsed += clock.restart();
+        world.update(clock.restart());
+        sf::sleep(sf::milliseconds(SLEEP_TIME));
     }
 
     return 0;
